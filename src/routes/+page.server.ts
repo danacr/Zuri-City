a706becf5c622c9767d7cff5edb19e6a69a4931b
danacr <dan@acristinii.com>
2023-04-25 19:11:13 +0200

    Initial version

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import type { PageServerLoad } from './$types';
+
+import Parser from 'rss-parser';
+
+
+export const load: PageServerLoad = async () => {
+    const parser: Parser = new Parser({})
+
+    const feed = await parser.parseURL('https://www.pls-zh.ch/plsFeed/rss');
+    feed.items.forEach(item => {
+        console.log(item.title + ':' + item.link) // item will have a `bar` property type as a number
+      });
+    return {
+        props: {
+            feed
+        }
+    };
+};
+
+
